3e98a6796b02f9b45a607f9e8a2feae31646d4b1
youjininreallife <gracecancho@gmail.com>
2022-03-07 10:54:24 +0900

    added Typography and Button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,25 @@
+import { Button, Container, Typography } from '@mui/material'
 import React from 'react'
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
+    <Container>
+      <Typography
+        variant='h6'
+        color='textSecondary'
+        component='h2'
+        gutterBottom
+      >
+        Create a New Code
+      </Typography>
+      <Button
+        type='submit'
+        color='secondary'
+        variant='contained'
+        onClick={() => alert('you clicked me')}
+      >
+        Submit
+      </Button>
+    </Container>
   )
 }
